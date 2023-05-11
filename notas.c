635e83ec765014206ae0dbf903e1753de9d11ac1
Yohannah Véras <yohannahlv@gmail.com>
2023-05-11 10:24:55 -0300

    first commit

---
+++ notas.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+  int main ()
+  {
+
+    double nota1, nota2, notaFinal;
+    
+    printf("Digite a primeira nota: ");
+    scanf("%lf", &nota1);
+    
+    printf("Digite a segunda nota: ");
+    scanf("%lf", &nota2);
+    
+    notaFinal = nota1 + nota2;
+    printf("Nota final: %.1lf\n", notaFinal);
+    
+    if(notaFinal < 60.00) {
+        printf("REPROVADO");
+    }
+    else{
+        printf("APROVADO");
+    }
+    
+    return 0;
+  }
